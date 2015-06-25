8d20557e446ecdce7f233e3c239937ceea50d4c1
Reiner Jung <rju@informatik.uni-kiel.de>
2015-06-25 19:38:01 +0200

    adding missing files

---
+++ de.cau.cs.se.geco.architecture/src-gen/de/cau/cs/se/geco/architecture/architecture/WriteTraceModel.java
@@ -0,0 +1,19 @@
+/**
+ */
+package de.cau.cs.se.geco.architecture.architecture;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Write Trace Model</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.cau.cs.se.geco.architecture.architecture.ArchitecturePackage#getWriteTraceModel()
+ * @model
+ * @generated
+ */
+public interface WriteTraceModel extends EObject
+{
+} // WriteTraceModel
