6c5ce7d02b2d7f9dbe723754ae63d045e13c1879
Reiner Jung <rju@informatik.uni-kiel.de>
2015-11-24 09:31:14 +0100

    fixed double introduction if ITraceModelProvider

--- de.cau.cs.se.geco.architecture.framework/src/de/cau/cs/se/geco/architecture/framework/ITraceModelProvider.xtend
+++ de.cau.cs.se.geco.architecture.framework/src/de/cau/cs/se/geco/architecture/framework/ITraceModelProvider.xtend
@@ -7,10 +7,20 @@ package de.cau.cs.se.geco.architecture.framework
  * @param <T> target model node type
  * @param <V> node type which is either <T> or a subtype of <T>
  */
-interface ITraceModelProvider<S,T> {
+interface ITraceModelProvider<S, T> {
+	/**
+	 * Add one trace to a trace model.
+	 */
 	def void add(S source, T target)
-
+	
+	/**
+	 * lookup all target nodes connected to the given source node. 
+	 */
 	def Iterable<T> lookup(S source)
-
+	
+	/**
+	 * lookup all target nodes connected to the given source node
+	 * which conform to a type V.
+	 */
 	def <V extends T> Iterable<V> lookup(S source, Class<V> clazz)
 }
--- de.cau.cs.se.geco.architecture.framework/xtend-gen/de/cau/cs/se/geco/architecture/framework/ITraceModelProvider.java
+++ de.cau.cs.se.geco.architecture.framework/xtend-gen/de/cau/cs/se/geco/architecture/framework/ITraceModelProvider.java
@@ -9,9 +9,19 @@ package de.cau.cs.se.geco.architecture.framework;
  */
 @SuppressWarnings("all")
 public interface ITraceModelProvider<S extends Object, T extends Object> {
+  /**
+   * Add one trace to a trace model.
+   */
   public abstract void add(final S source, final T target);
   
+  /**
+   * lookup all target nodes connected to the given source node.
+   */
   public abstract Iterable<T> lookup(final S source);
   
+  /**
+   * lookup all target nodes connected to the given source node
+   * which conform to a type V.
+   */
   public abstract <V extends T> Iterable<V> lookup(final S source, final Class<V> clazz);
 }
