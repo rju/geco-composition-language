4950483bb6d3e17b2acec5feea728b53e0c45ee0
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-05-14 10:38:02 +0200

    fixed dependencies

--- de.cau.cs.se.geco.architecture.tests/src/de/cau/cs/se/geco/architecture/tests/ArchitectureParsingTest.xtend
+++ de.cau.cs.se.geco.architecture.tests/src/de/cau/cs/se/geco/architecture/tests/ArchitectureParsingTest.xtend
@@ -5,14 +5,15 @@ package de.cau.cs.se.geco.architecture.tests
 
 import com.google.inject.Inject
 import de.cau.cs.se.geco.architecture.architecture.GecoModel
-import org.eclipse.xtext.junit4.InjectWith
-import org.eclipse.xtext.junit4.XtextRunner
-import org.eclipse.xtext.junit4.util.ParseHelper
+import org.eclipse.xtext.testing.InjectWith
+import org.eclipse.xtext.testing.XtextRunner
+import org.eclipse.xtext.testing.util.ParseHelper
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(XtextRunner)
+@InjectWith(ArchitectureInjectorProvider)
 class ArchitectureParsingTest{
 
 	@Inject
@@ -21,7 +22,7 @@ class ArchitectureParsingTest{
 	@Test 
 	def void loadModel() {
 		val result = parseHelper.parse('''
-			Hello Xtext!
+			package example
 		''')
 		Assert.assertNotNull(result)
 	}
--- de.cau.cs.se.geco.architecture.tests/xtend-gen/de/cau/cs/se/geco/architecture/tests/ArchitectureParsingTest.java
+++ de.cau.cs.se.geco.architecture.tests/xtend-gen/de/cau/cs/se/geco/architecture/tests/ArchitectureParsingTest.java
@@ -5,15 +5,18 @@ package de.cau.cs.se.geco.architecture.tests;
 
 import com.google.inject.Inject;
 import de.cau.cs.se.geco.architecture.architecture.GecoModel;
+import de.cau.cs.se.geco.architecture.tests.ArchitectureInjectorProvider;
 import org.eclipse.xtend2.lib.StringConcatenation;
-import org.eclipse.xtext.junit4.XtextRunner;
-import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.XtextRunner;
+import org.eclipse.xtext.testing.util.ParseHelper;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(XtextRunner.class)
+@InjectWith(ArchitectureInjectorProvider.class)
 @SuppressWarnings("all")
 public class ArchitectureParsingTest {
   @Inject
@@ -23,7 +26,7 @@ public class ArchitectureParsingTest {
   public void loadModel() {
     try {
       StringConcatenation _builder = new StringConcatenation();
-      _builder.append("Hello Xtext!");
+      _builder.append("package example");
       _builder.newLine();
       final GecoModel result = this.parseHelper.parse(_builder);
       Assert.assertNotNull(result);
